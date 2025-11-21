3e4e3b71b82643fd05e404888a6d86591ee41eb4
krishna1369 <100772377+krishna1369@users.noreply.github.com>
2025-11-22 00:39:42 +0530

    Update data.js

--- Code/src/data.js
+++ Code/src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Build and Deployment has been successfully",
+    title: "Build and Deployment has been successful by Jinkins",
     desc: "This is now in new commit",
     bg: "green",
   },
